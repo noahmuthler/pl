c240147d186ee3fe37bb6c8f5cfcfc0594b53cfb
noahmuthler <noahmuthler@gmail.com>
2021-02-15 17:19:48 -0500

    changed some router functions

--- src/index.js
+++ src/index.js
@@ -3,5 +3,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import SignUp from "./SignUp";
 import App from "./App";
+import { BrowserRouter } from 'react-router-dom';
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<BrowserRouter basename={process.env.PUBLIC_URL}><App /></BrowserRouter>, document.getElementById("root"));
