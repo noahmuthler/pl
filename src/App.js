643fba93e2d791c2f1172782f369544ad464bb31
noahmuthler <noahmuthler@gmail.com>
2021-02-14 23:37:18 -0500

    help me

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import { BrowserRouter, Link, Route, Switch } from 'react-router-dom';
+import SignUp from "../src/SignUp";
+import HomeNav from "./components/Home/HomeNav";
+import AboutUs from "./components/Home/AboutUs";
 
 function App() {
-  return (
-    <div className="App">
-      <h1>I really hate github pages</h1>
-    </div>
-  );
+    return (
+      <BrowserRouter>
+        <div>
+            <Switch>
+              <Route path="/" exact component={HomeNav}/>
+              <Route path="/signup" component={SignUp}/>
+              <Route path="/about" component={AboutUs}/>
+            </Switch>
+        </div>
+      </BrowserRouter>
+    );
 }
 
 export default App;
